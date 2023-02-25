cf77ec49d8c112bcb6e4fcb0b39802272e9f325d
midicable <bogdan.astronomy@gmail.com>
2023-02-26 00:39:22 +0300

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = Math.round((this.min + this.max) / 2);
     }
 
     greater() {
-
+        this.min = Math.round((this.min + this.max) / 2);
     }
 }
 
 module.exports = GuessingGame;
+     
